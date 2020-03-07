f3a1f17a6ea925489afbcbe699aaef8b63218a03
Tuan Ngo <tuannm79@gmail.com>
2020-03-07 11:23:11 +0700

    chore(ci): test code change (#3)
    * test code change
    
    * test code change

--- src/app/components/shell/shell.component.ts
+++ src/app/components/shell/shell.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   template: `
     <mat-toolbar color="primary">
       <mat-toolbar-row>
-        <span>Training Plans</span>
+        <span>Training Plans..</span>
         <span class="example-spacer"></span>
         <mat-icon class="example-icon" aria-hidden="false" aria-label="Add Todo">add_box</mat-icon>
         <mat-icon class="example-icon" aria-hidden="false" aria-label="Logout">power_settings_new</mat-icon>
